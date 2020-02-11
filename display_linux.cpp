d49bea9c595704c1c8e740e091a6572437f8a4e9
D0023R <site.github@undercover.mine.nu>
2020-02-11 10:58:26 +0100

    Xorg error handling done + clean-up

--- display_linux.cpp
+++ display_linux.cpp
@@ -24,51 +24,10 @@ bool linux_display::init(display_settings *ds)
 	log_verbose("AWK: display_linux init\n");
 
 	set_factory(new custom_video);
-	set_custom_video(factory()->make(ds->screen, NULL, NULL, NULL));
+	set_custom_video(factory()->make(ds->screen, NULL, CUSTOM_VIDEO_TIMING_XRANDR, NULL));
 	if (video()) video()->init();
 
 	return true;
 }
 
-//============================================================
-//  linux_display::get_desktop_mode
-//============================================================
-
-bool linux_display::get_desktop_mode()
-{
-	log_verbose("AWK: display_linux get_desktop_mode\n");
-	return true;
-}
-
-//============================================================
-//  linux_display::set_desktop_mode
-//============================================================
-
-bool linux_display::set_desktop_mode(modeline *mode, int flags)
-{
-	log_verbose("AWK: display_linux set_desktop_mode\n");
-	//if (video()) video()->set_custom_video_mode(mode);
-	return true;
-}
-
-//============================================================
-//  linux_display::restore_desktop_mode
-//============================================================
-
-bool linux_display::restore_desktop_mode()
-{
-	log_verbose("AWK: display_linux restore_desktop_mode\n");
-	//if (video()) video()->modeline_reset();
-	return true;
-}
-
-//============================================================
-//  linux_display::get_available_video_modes
-//============================================================
-
-int linux_display::get_available_video_modes()
-{
-	log_verbose("AWK: display_linux get_available_video_mode\n");
-	return false;
-}
 
