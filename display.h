da818619778fa7056787e53d4791f4f60ade297c
Antonio Giner <calamity15khz@gmail.com>
2022-01-04 19:10:45 +0100

    Add some missing getters & setters to display manager.

--- display.h
+++ display.h
@@ -109,6 +109,12 @@ public:
 	bool is_mode_updated() { return m_best_mode != nullptr? m_best_mode->type & MODE_UPDATE : false; }
 	bool is_mode_new() { return m_best_mode != nullptr? m_best_mode->type & MODE_ADD : false; }
 
+	// getters (custom_video backend)
+	bool screen_compositing() { return m_ds.vs.screen_compositing; }
+	bool screen_reordering() { return m_ds.vs.screen_reordering; }
+	bool allow_hardware_refresh() { return m_ds.vs.allow_hardware_refresh; }
+	const char *custom_timing() { return (const char*) &m_ds.vs.custom_timing; }
+
 	// setters
 	void set_index(int index) { m_index = index; }
 	void set_factory(custom_video *factory) { m_factory = factory; }
@@ -143,6 +149,12 @@ public:
 	void set_v_shift_correct(int value) { m_ds.gs.v_shift_correct = value; }
 	void set_pixel_precision(int value) { m_ds.gs.pixel_precision = value; }
 	void set_interlace_force_even(int value) { m_ds.gs.interlace_force_even = value; }
+
+	// setters (custom_video backend)
+	void set_screen_compositing(bool value) { m_ds.vs.screen_compositing = value; }
+	void set_screen_reordering(bool value) { m_ds.vs.screen_reordering = value; }
+	void set_allow_hardware_refresh(bool value) { m_ds.vs.allow_hardware_refresh = value; }
+	void set_custom_timing(const char *custom_timing) { strncpy(m_ds.vs.custom_timing, custom_timing, sizeof(m_ds.vs.custom_timing)-1); }
 
 	// options
 	display_settings m_ds = {};
